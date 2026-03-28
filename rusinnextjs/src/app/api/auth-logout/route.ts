ba2bf234f000ccaa5bbdaf41ed8328b73d32c4c2
arsalanshaikh13 <m.arsalanshaikh13@gmail.com>
2026-03-28 19:11:54 +0530

    updated auth-logout/route.ts with hard coded domain name

--- rusinnextjs/src/app/api/auth-logout/route.ts
+++ rusinnextjs/src/app/api/auth-logout/route.ts
@@ -2,5 +2,6 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   // Catch the user returning from Cognito and immediately bounce them to the homepage
-  return NextResponse.redirect(new URL("/", request.url));
+  //   return NextResponse.redirect(new URL("/", request.url));
+  return NextResponse.redirect(new URL("/", "https://devsandbox.space"));
 }
